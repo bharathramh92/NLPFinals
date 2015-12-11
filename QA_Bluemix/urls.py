61b2ec7777958a6aa38f2093598373f3509c2f30
bharath <bharathramh92@gmail.com>
2015-12-10 21:24:17 -0500

    updated single data to data from database(aws rds)

--- QA_Bluemix/urls.py
+++ QA_Bluemix/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('portal.urls', namespace="index")),
+    url(r'^items/', include('portal.urls', namespace="index")),
 ]
--- portal/views.py
+++ portal/views.py
@@ -5,27 +5,20 @@ from portal.Constants import *
 import requests
 import nltk
 import json
+from nltk.stem.wordnet import WordNetLemmatizer
+from review_model_helper.models import ProductReviews, Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from portal.paraSumm import summarize
 
 
-def ie_preprocess(keyWords):
-    with open("sample_review_baby.json") as fl:
-        data = json.loads(fl.read())
-        document = ''.join([dt["reviewText"] for dt in data])
-        # print(document)
-        sentences = nltk.sent_tokenize(document)
-        # print(sentences)
-        document = [nltk.word_tokenize(sent) for sent in sentences]
-        print(document)
-        answers = []
-        for sent in document:
-            for keyword in keyWords:
-                if keyword in sent:
-                    print(sent)
-                    answers.append(' '.join(sent))
-        return answers
+def get_product_reviews(product_id):
+    reviews = ProductReviews.objects.filter(product_id=product_id)
+    document = ''.join([review.review_text for review in reviews])
+    print(document)
+    return document
 
 
-def req_rank_retrieve(qn):
+def req_rank_retrieve(qn, product_id):
     params['text'] = qn
     r = requests.get(ALCHEMY_URL, params=params)
     data = r.json()
@@ -35,26 +28,58 @@ def req_rank_retrieve(qn):
             keyWords.append(dt['text'])
     except KeyError:
         print("KeyError")
-    print(keyWords)
-    return ie_preprocess(keyWords)
+    print("keywords, ", keyWords)
+    lemmatizer = WordNetLemmatizer()
+    document = get_product_reviews(product_id)
+    sentences = nltk.sent_tokenize(document)
+    document = []
+    for sent in sentences:
+        sent_list = []
+        for word in nltk.word_tokenize(sent):
+            sent_list.append(word)
+        document.append(sent_list)
+    answers = []
+    and_sent = set()
+    for sent_i in range(0, len(document)):
+        sent = document[sent_i]
+        sent_lemma = []
+        for st in sent:
+            sent_lemma.append(lemmatizer.lemmatize(st))
+        for keyword in keyWords:
+            if lemmatizer.lemmatize(keyword) in sent_lemma:
+                and_sent.add(sent_i)
+    for s_id in and_sent:
+        answers.append(' '.join(document[s_id]))
+    print(answers)
+    answers = ''.join(answers)
+    return ''.join(summarize(answers))
 
 
-def index_view(request):
-    # if this is a POST request we need to process the form data
+def product_view(request, product_id):
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
         form = QuestionForm(request.POST)
-        # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
             qn = form.cleaned_data['question']
-            answers = req_rank_retrieve(qn)
+            answers = req_rank_retrieve(qn, product_id)
             return render(request, 'portal/answer.html', {'answers': answers})
-
-    # if a GET (or any other method) we'll create a blank form
     else:
         form = QuestionForm()
 
-    return render(request, 'portal/index.html', {'form': form})
+    return render(request, 'portal/product.html', {'form': form, 'product_id':product_id})
+
+
+def index_view(request):
+    product_list = Product.objects.all()
+    paginator = Paginator(product_list, 25) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        products = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        products = paginator.page(paginator.num_pages)
+
+    return render(request, 'portal/index.html', {"products": products})
